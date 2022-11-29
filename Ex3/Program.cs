87a8ec795f80a2fb047a796cfa1fc6018483e1a3
Michael_SVAO <vierumihail7@gmail.com>
2022-11-29 22:51:32 +0300

    Added new Random

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -1,6 +1,6 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); // 1, 2, 3, 4, ... 9 Слово Next говорит о том что число у нас целое
+Console.WriteLine(numberA);
+int numberB = new Random().Next (1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
-
-
